ca63fb38379b1bd7074c3a56b1bc9ec6b4a97d5b
Rouven Himmelstein <rouven@himmelstein.info>
2023-05-18 19:59:25 +0200

    fix: file upload

--- .github/scripts/upload-asset-to-release.sh
+++ .github/scripts/upload-asset-to-release.sh
@@ -3,6 +3,9 @@ set -e
 # This script uploads a file to the latest release of the repository
 # It requires the GitHub Personal Access Token
 
+# The repository name in the format of user/repo
+REPO_NAME=RouHim/rouvens-arch-kickstart
+
 # The first argument is the GitHub Personal Access Token
 GITHUB_TOKEN=$1
 
@@ -12,15 +15,16 @@ BIN_PATH=$2
 # The third argument is the name of the asset's file in the release
 NAME=$3
 
+
 echo "Uploading $BIN_PATH to $NAME"
 
 # Get the release ID from the latest release
-RELEASE_ID=$(curl -s https://api.github.com/repos/RouHim/this-week-in-past/releases/latest | jq -r '.id')
+RELEASE_ID=$(curl -s https://api.github.com/repos/${REPO_NAME}/releases/latest | jq -r .id)
 
 echo "Release ID: $RELEASE_ID"
 
 # Upload the asset
-UPLOAD_URL="https://uploads.github.com/repos/RouHim/rouvens-arch-kickstart/releases/${RELEASE_ID}/assets?name=${NAME}"
+UPLOAD_URL="https://uploads.github.com/repos/${REPO_NAME}/releases/${RELEASE_ID}/assets?name=${NAME}"
 curl -X POST \
   -H "Content-Type: $(file -b --mime-type "$BIN_PATH")" \
   -H "Authorization: token ${GITHUB_TOKEN}" \
