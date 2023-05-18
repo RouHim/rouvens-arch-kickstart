7c4a587dbe79d175f51decf803ec3ec33cdca4f0
Rouven Himmelstein <rouven@himmelstein.info>
2023-05-18 19:26:12 +0200

    feat: github actions release

---
+++ .github/scripts/upload-asset-to-release.sh
@@ -0,0 +1,26 @@
+#!/bin/env bash
+set -e
+# This script uploads a file to the latest release of the repository
+# It requires the GitHub Personal Access Token
+
+# The first argument is the GitHub Personal Access Token
+GITHUB_TOKEN=$1
+
+# The second argument is the path to the asset's file
+BIN_PATH=$2
+
+# The third argument is the name of the asset's file in the release
+NAME=$3
+
+echo "Uploading $BIN_PATH to $NAME"
+
+# Get the release ID from the latest release
+RELEASE_ID=$(curl -s https://api.github.com/repos/RouHim/this-week-in-past/releases/latest | jq -r '.id')
+
+# Upload the asset
+UPLOAD_URL="https://uploads.github.com/repos/RouHim/this-week-in-past/releases/${RELEASE_ID}/assets?name=${NAME}"
+curl -X POST \
+  -H "Content-Type: $(file -b --mime-type "$BIN_PATH")" \
+  -H "Authorization: token ${GITHUB_TOKEN}" \
+  -T "${BIN_PATH}" \
+  "${UPLOAD_URL}"
