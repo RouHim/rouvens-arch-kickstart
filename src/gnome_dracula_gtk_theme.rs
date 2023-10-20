e88c3e0b2946e9920acd3af824999b4502e6f4f1
Rouven Himmelstein <rouven@himmelstein.info>
2023-10-20 07:53:03 +0200

    fix(packages): apply correct v40 gnome shell styles

--- src/gnome_dracula_gtk_theme.rs
+++ src/gnome_dracula_gtk_theme.rs
@@ -1,5 +1,4 @@
 use std::fs;
-
 use std::path::PathBuf;
 
 use crate::shell::RootShell;
@@ -12,6 +11,8 @@ pub struct GnomeDraculaGtkTheme {}
 const THEME_PACKAGE_NAME: &str = "dracula-gtk-theme-git";
 const ICONS_PACKAGE_NAME: &str = "dracula-icons-git";
 const CURSOR_PACKAGE_NAME: &str = "dracula-cursors-git";
+const GTK_THEME_INSTALL_PATH: &str = "/usr/share/themes/Dracula/gnome-shell";
+const GTK_THEME_INSTALL_PATH_V40: &str = "/usr/share/themes/Dracula/gnome-shell/v40";
 
 impl Feature for GnomeDraculaGtkTheme {
     fn install(&self, root_shell: &mut RootShell) -> bool {
@@ -27,6 +28,20 @@ impl Feature for GnomeDraculaGtkTheme {
         {
             root_shell.execute("echo 'GTK_THEME=Dracula' >> /etc/environment");
         }
+
+        // Apply the gnome shell v40 themes to default styling
+        if fs::metadata(GTK_THEME_INSTALL_PATH_V40).is_ok() {
+            root_shell.execute(format!(
+                "cp -r {GTK_THEME_INSTALL_PATH_V40}/* {GTK_THEME_INSTALL_PATH}"
+            ));
+        }
+
+        // Activate dracula theme everywhere
+        root_shell.execute("gsettings set org.gnome.desktop.interface gtk-theme 'Dracula'");
+        root_shell.execute("gsettings set org.gnome.desktop.interface icon-theme 'Dracula'");
+        root_shell.execute("gsettings set org.gnome.desktop.interface cursor-theme 'Dracula'");
+        root_shell.execute("gsettings set org.gnome.desktop.wm.preferences theme 'Dracula'");
+        root_shell.execute("gsettings set org.gnome.shell.extensions.user-theme name 'Dracula'");
 
         GnomeDraculaGtkTheme::is_installed(self)
     }
