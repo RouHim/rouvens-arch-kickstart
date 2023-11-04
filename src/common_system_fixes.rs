6e4398864d5bec1c53e8d3690b289b082fbc8ab0
Rouven Himmelstein <rouven@himmelstein.info>
2023-11-04 12:36:43 +0100

    feat(packages): limit journald log size

--- src/common_system_fixes.rs
+++ src/common_system_fixes.rs
@@ -12,8 +12,12 @@ pub struct CommonSystemFixes {}
 impl Feature for CommonSystemFixes {
     fn install(&self, root_shell: &mut RootShell) -> bool {
         // Set MOZ_ENABLE_WAYLAND=1 in /etc/environment
-        // using root shell
-        root_shell.execute("echo 'MOZ_ENABLE_WAYLAND=1' >> /etc/environment");
+        if !root_shell.execute("grep -q MOZ_ENABLE_WAYLAND=1 /etc/environment") {
+            root_shell.execute("echo 'MOZ_ENABLE_WAYLAND=1' >> /etc/environment");
+        }
+
+        // Limit journald size, create a new file in /etc/systemd/journald.conf.d/
+        Self::limit_journald_log_size(root_shell);
 
         CommonSystemFixes::is_installed(self)
     }
@@ -23,6 +27,9 @@ impl Feature for CommonSystemFixes {
         // using root shell
         root_shell.execute("sed -i '/MOZ_ENABLE_WAYLAND=1/d' /etc/environment");
 
+        // Remove the journald limit file
+        root_shell.execute("rm -rf /etc/systemd/journald.conf.d/limit.conf");
+
         !CommonSystemFixes::is_installed(self)
     }
 
@@ -31,10 +38,28 @@ impl Feature for CommonSystemFixes {
         let environment_file = PathBuf::from("/etc/environment");
         let environment_file_contents = fs::read_to_string(environment_file).unwrap();
 
-        environment_file_contents.contains("MOZ_ENABLE_WAYLAND=1")
+        // Check if limit.conf exists
+        let limit_file_exists = PathBuf::from("/etc/systemd/journald.conf.d/limit.conf").exists();
+
+        limit_file_exists && environment_file_contents.contains("MOZ_ENABLE_WAYLAND=1")
     }
 
     fn get_name(&self) -> String {
         String::from("Apply common system fixes")
+    }
+}
+
+impl CommonSystemFixes {
+    fn limit_journald_log_size(root_shell: &mut RootShell) {
+        // Remove file if exists
+        root_shell.execute("rm -rf /etc/systemd/journald.conf.d/limit.conf");
+
+        root_shell.execute("mkdir -p /etc/systemd/journald.conf.d/");
+        root_shell.execute("echo '[Journal]' > /etc/systemd/journald.conf.d/limit.conf");
+        root_shell.execute("echo 'SystemMaxUse=50M' >> /etc/systemd/journald.conf.d/limit.conf");
+        root_shell
+            .execute("echo 'SystemMaxFileSize=10M' >> /etc/systemd/journald.conf.d/limit.conf");
+        root_shell.execute("echo 'SystemKeepFree=100M' >> /etc/systemd/journald.conf.d/limit.conf");
+        root_shell.execute("echo 'SystemMaxFiles=5' >> /etc/systemd/journald.conf.d/limit.conf");
     }
 }
